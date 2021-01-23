44de3ba6a0290c7130ee6bb88f0c2aa954899e3e
PedroHCT <pedro.hct1708@hotmail.com>
2021-01-23 17:10:12 -0300

    jogo-dinossauro part1

--- JavascriptEC6/parte4.js
+++ JavascriptEC6/parte4.js
@@ -3,8 +3,34 @@
 -- Parte 4 -- 
 
 
-*/
 
+
+**Promises**
+
+const doSomethingPromise = () => new Promise((resolve, reject) => {
+    try {
+        setTimeout(function () {
+            console.log('First');
+        }, 800);
+    } catch (error) {
+        //handle error
+    }
+});
+const doOtherThingPromise = () => new Promise((resolve, reject) => {
+    try {
+        setTimeout(function () {
+            console.log('Second');
+        }, 1000);
+    } catch (error) {
+        //handle error
+    }
+});
+
+Promise.race([doSomethingPromise(), doOtherThingPromise()]).then(data => {
+    console.log(data);
+});
+
+**callbacks**
 function doSomething(callback) {
     setTimeout(function () {
         callback('First Data');
@@ -25,10 +51,13 @@ function doAll() {
             try {
                 doOtherThing(function (data2) {
                     var processedData2 = data2;
-
-                    setTimeout(function () {
-                        console.log(processedData + '\n' + processedData2);
-                    }, 1000);
+                    try {
+                        setTimeout(function () {
+                            console.log(processedData + '\n' + processedData2);
+                        }, 1000);
+                    } catch (error) {
+                        //handle error
+                    }
                 });
             } catch (error) {
                 //handle error
@@ -40,3 +69,39 @@ function doAll() {
 }
 
 doAll();
+
+
+
+**async & await**
+const asyncTimer = () => new Promise((resolve, reject) => {
+    setTimeout(() =>{
+        resolve('teste asyncTimer');
+    }, 1000);
+});
+
+const simpleFunc = async () =>{
+    const data = await asyncTimer();
+    return data;
+};
+
+simpleFunc().then(data =>{
+    console.log(data);
+}).catch(error =>{
+    console.log(error);
+});
+
+
+
+** Event Emitter **
+
+*/
+
+const EventEmitter = require('events');
+
+const emitter = new EventEmitter();
+
+emitter.on('User logged', data =>{
+    console.log(data);
+});
+
+emitter.emit('User logged', {user: 'Celso Henrique'});
